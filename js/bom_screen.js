d8dbbd007fcc8e0c504e93bd6b8d199ca844ba4d
diegops17 <psdiego17@gmail.com>
2024-04-02 22:21:58 -0300

    bom, window e screen

---
+++ js/bom_screen.js
@@ -0,0 +1,11 @@
+let altura = window.screen.availHeight
+let largura = window.screen.availWidth
+
+//document.write('Altura: ', altura,'<br/>')
+//document.write('Largura: ', largura)
+
+if(largura < 500){
+    document.write('Tela menor que 500px')
+}else{
+    document.write('Tela maior que 500px')
+}
---
+++ js/bom_window.js
@@ -0,0 +1,9 @@
+let nova_janela
+
+function abrirPouUp(){
+    janela = window.open('http://www.google.com', 'nova_janela', 'width=200',  'height=200')
+}
+
+function fecharPouUp(){
+    janela.close()
+}
